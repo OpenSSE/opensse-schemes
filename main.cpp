21b8517ab97ea706fb0a394433e271503416a1e9
Raphael Bost <raphael_bost@alumni.brown.edu>
2016-03-29 18:30:42 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sophos
+//
+//  Created by Raphael Bost on 29/03/2016.
+//  Copyright © 2016 Raphael Bost. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

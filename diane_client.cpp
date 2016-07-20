122bf2123b643d59606340fd5711edb457864f9d
Raphael Bost <raphael_bost@alumni.brown.edu>
2016-07-20 13:55:20 +0200

    Functionnal Diane client. The client-server couple works, but is still buggy

--- diane_client.cpp
+++ diane_client.cpp
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Raphael Bost. All rights reserved.
 //
 
-//#include "sophos/sophos_client_runner.hpp"
+#include "diane/client_runner.hpp"
 #include "logger.hpp"
 #include "aux/db_generator.hpp"
 
@@ -95,86 +95,89 @@ int main(int argc, char** argv) {
         sse::logger::log(sse::logger::INFO) << "Running client with database " << client_db << std::endl;
     }
     
-//    std::unique_ptr<sse::sophos::SophosClientRunner> client_runner;
+    std::unique_ptr<sse::diane::DianeClientRunner> client_runner;
     
-//    if (json_file.size() > 0) {
-//        client_runner.reset( new sse::sophos::SophosClientRunner("localhost:4242", client_db, json_file) );
-//    }else{
-//        size_t setup_size = 1e5;
-//        uint32_t n_keywords = 1e4;
-//        
-//        if( rnd_entries_count > 0)
-//        {
-//            setup_size = 11*rnd_entries_count;
-//            n_keywords = 1.4*rnd_entries_count/(10*std::thread::hardware_concurrency());
-//        }
-//        
-//        client_runner.reset( new sse::sophos::SophosClientRunner("localhost:4242", client_db, setup_size, n_keywords) );
-//    }
-//
-//    for (std::string &path : input_files) {
-//        sse::logger::log(sse::logger::INFO) << "Load file " << path << std::endl;
-//        client_runner->load_inverted_index(path);
-//        sse::logger::log(sse::logger::INFO) << "Done loading file " << path << std::endl;
-//    }
-//    
-//    if (rnd_entries_count > 0) {
-//        sse::logger::log(sse::logger::INFO) << "Randomly generating database with " << rnd_entries_count << " docs" << std::endl;
-//        
-////        auto post_callback = [&writer, &res_size, &writer_lock](index_type i)
-//
-//        auto gen_callback = [&client_runner](const std::string &s, size_t i)
-//        {
-//            client_runner->async_update(s, i);
-//        };
-//        
-//        client_runner->start_update_session();
-//        sse::sophos::gen_db(rnd_entries_count, gen_callback);
-//        client_runner->end_update_session();
-//    }
+    if (json_file.size() > 0) {
+        sse::logger::log(sse::logger::CRITICAL) << "JSON import is not supported in Diane!" << std::endl;
+//        client_runner.reset( new sse::diane::DianeClientRunner("localhost:4241", client_db, json_file) );
+    }else{
+        size_t setup_size = 1e5;
+        uint32_t n_keywords = 1e4;
+        
+        if( rnd_entries_count > 0)
+        {
+            setup_size = 11*rnd_entries_count;
+            n_keywords = 1.4*rnd_entries_count/(10*std::thread::hardware_concurrency());
+        }
+        
+        client_runner.reset( new sse::diane::DianeClientRunner("localhost:4241", client_db, setup_size, n_keywords) );
+    }
+
+    for (std::string &path : input_files) {
+        sse::logger::log(sse::logger::INFO) << "Load file " << path << std::endl;
+        client_runner->load_inverted_index(path);
+        sse::logger::log(sse::logger::INFO) << "Done loading file " << path << std::endl;
+    }
     
-//    for (std::string &kw : keywords) {
-//        std::cout << "-------------- Search --------------" << std::endl;
-//        
-//        std::mutex logger_mtx;
-//        std::ostream& log_stream = sse::logger::log(sse::logger::INFO);
-//        bool first = true;
-//        
-//        auto print_callback = [&logger_mtx, &log_stream, &first](uint64_t res)
-//        {
-//            // logger_mtx.lock();
-//            //
-//            // if (!first) {
-//            //     log_stream << ", ";
-//            // }
-//            // first = false;
-//            // log_stream << res;
-//            //
-//            // logger_mtx.unlock();
-//        };
-//        
-//        log_stream << "Search results: \n{";
-//
-//        auto res = client_runner->search(kw, print_callback);
-//        
-//        log_stream << "}" << std::endl;
-//    }
-//    
-//    if (bench_count > 0) {
-//        std::cout << "-------------- Search Benchmarks --------------" << std::endl;
-//        client_runner->search_benchmark(bench_count);
-//    }
-//    
-//    if (output_path.size()>0) {
+    if (rnd_entries_count > 0) {
+        sse::logger::log(sse::logger::INFO) << "Randomly generating database with " << rnd_entries_count << " docs" << std::endl;
+        
+//        auto post_callback = [&writer, &res_size, &writer_lock](index_type i)
+
+        auto gen_callback = [&client_runner](const std::string &s, size_t i)
+        {
+            client_runner->async_update(s, i);
+        };
+        
+        client_runner->start_update_session();
+        sse::sophos::gen_db(rnd_entries_count, gen_callback);
+        client_runner->end_update_session();
+    }
+    
+    for (std::string &kw : keywords) {
+        std::cout << "-------------- Search --------------" << std::endl;
+        
+        std::mutex logger_mtx;
+        std::ostream& log_stream = sse::logger::log(sse::logger::INFO);
+        bool first = true;
+        
+        auto print_callback = [&logger_mtx, &log_stream, &first](uint64_t res)
+        {
+             logger_mtx.lock();
+            
+             if (!first) {
+                 log_stream << ", ";
+             }
+             first = false;
+             log_stream << res;
+            
+             logger_mtx.unlock();
+        };
+        
+        log_stream << "Search results: \n{";
+
+        auto res = client_runner->search(kw, print_callback);
+        
+        log_stream << "}" << std::endl;
+    }
+    
+    if (bench_count > 0) {
+        std::cout << "-------------- Search Benchmarks --------------" << std::endl;
+        client_runner->search_benchmark(bench_count);
+    }
+    
+    if (output_path.size()>0) {
+        sse::logger::log(sse::logger::CRITICAL) << "JSON export is not supported in Diane!" << std::endl;
+
 //        client_runner->output_db(output_path);
-//    }
-//    
-//    if (print_stats)
-//    {
-//        client_runner->print_stats(sse::logger::log(sse::logger::INFO));
-//    }
-//    
-//    client_runner.reset();
+    }
+    
+    if (print_stats)
+    {
+        client_runner->print_stats(sse::logger::log(sse::logger::INFO));
+    }
+    
+    client_runner.reset();
     
     sse::crypto::cleanup_crypto_lib();
 

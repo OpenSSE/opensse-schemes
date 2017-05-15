1fdbb8b68589e90edc237dc9fff03e1bfeb58910
Raphael Bost <raphael_bost@alumni.brown.edu>
2017-05-15 22:26:31 +0200

    Also compute min and max

--- script_bench/graph_gen.py
+++ script_bench/graph_gen.py
@@ -2,7 +2,7 @@
 
 # from optparse import OptionParser
 import argparse
-
+import math
 
 
 def parse_file(f):
@@ -10,10 +10,11 @@ def parse_file(f):
     # with open(filepath) as f:
     lines = f.readlines()
     for l in lines:
-        tokens = l.strip('\n').split(' ')
-        keyword = tokens[0]
-        value = float(tokens[2])
+        tokens = l.strip('\n\t').split(' \t')
         # print tokens
+
+        keyword = tokens[0]
+        value = float(tokens[1])
         # print 'keyword: {0}'.format(keyword)
         # print 'value: {0}'.format(value)
         
@@ -21,24 +22,32 @@ def parse_file(f):
             old_sum = stats[keyword]["sum"]
             old_square_sum = stats[keyword]["square_sum"]
             counter = stats[keyword]["counter"]
+            min_v = stats[keyword]["min"]
+            max_v = stats[keyword]["max"]
         else:
             old_sum = 0
             old_square_sum = 0
             counter = 0
+            min_v = 1e9
+            max_v = 0
             stats[keyword] = dict()
             
         stats[keyword]["sum"] = old_sum+value
         stats[keyword]["square_sum"] = old_square_sum + value*value
         stats[keyword]["counter"] = counter+1
+        stats[keyword]["min"] = min(min_v, value)
+        stats[keyword]["max"] = max(max_v, value)
     
     return stats
     
 def compute_mean_var(stats):
     res = {}
     for key in stats:
-        res[int(key)] = {}
-        res[int(key)]["mean"] = stats[key]["sum"]/stats[key]["counter"]
-        res[int(key)]["var"] = stats[key]["square_sum"]/stats[key]["counter"]
+        res[key] = {}
+        res[key]["mean"] = stats[key]["sum"]/stats[key]["counter"]
+        res[key]["dev"] = math.sqrt(stats[key]["square_sum"]/stats[key]["counter"] - res[key]["mean"]*res[key]["mean"])
+        res[key]["min"] = stats[key]["min"]
+        res[key]["max"] = stats[key]["max"]
 
     return res
 
@@ -46,7 +55,7 @@ def compute_mean_var(stats):
 def print_mean_var(stats, f):
     # with open(filepath, 'w') as f:
     for key in sorted(stats.iterkeys()):
-        line = '{0} \t {1} \t {2}\n'.format(key, stats[key]["mean"], stats[key]["var"])
+        line = '{0} \t {1} \t {2} \t {3} \t {4}\n'.format(key, stats[key]["mean"], stats[key]["dev"], stats[key]["min"], stats[key]["max"])
         f.write(line)
     f.close()
             

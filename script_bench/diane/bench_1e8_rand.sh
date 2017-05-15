1ae15b269f1aa0a92320790990f813db63a54760
Raphael Bost <raphael_bost@alumni.brown.edu>
2017-05-15 21:41:24 +0200

    Improved python script to generate graph data

--- script_bench/diane/bench_1e8_rand.sh
+++ script_bench/diane/bench_1e8_rand.sh
@@ -8,57 +8,68 @@ kKeyword10GroupBase=$kKeywordGroupBase"10^"
 kKeywordRand10GroupBase=$kKeywordGroupBase"rand-10^"
 
 
-for i in `seq 0 7`;
-do
-	for j in `seq 0 124`; #total number is 1000
-	do
-		kw_list=$kw_list" "$kKeyword10GroupBase"1_"$i"_"$j
-	done
-done
-
-./diane_client -q -b $db_file $kw_list
-
-kw_list=""
-for i in `seq 0 7`;
-do
-	for j in `seq 0 124`; #total number is 1000
-	do
-		kw_list=$kw_list" "$kKeyword10GroupBase"2_"$i"_"$j
-	done
-done
-./diane_client -q -b $db_file $kw_list
-
-kw_list=""
-for i in `seq 0 7`;
-do
-	for j in `seq 0 124`; #total number is 1000
-	do
-		kw_list=$kw_list" "$kKeyword10GroupBase"3_"$i"_"$j
-	done
-done
-./diane_client -q -b $db_file $kw_list
-
-kw_list=""
-for i in `seq 0 7`;
-do
-	for j in `seq 0 124`;
-	do
-		kw_list=$kw_list" "$kKeyword10GroupBase"4_"$i"_"$j
-	done
-done
-./diane_client -q -b $db_file $kw_list
-
-kw_list=""
-for i in `seq 0 7`;
-do
-        for j in `seq 0 124`;
-        do
-                kw_list=$kw_list" "$kKeyword10GroupBase"5_"$i"_"$j
-        done
-done
-./diane_client -q -b $db_file $kw_list
+# for i in `seq 0 7`;
+# do
+# 	for j in `seq 0 124`; #total number is 1000
+# 	do
+# 		kw_list=$kw_list" "$kKeyword10GroupBase"1_"$i"_"$j
+# 	done
+# done
+#
+# ./diane_client -q -b $db_file $kw_list
+#
+# kw_list=""
+# for i in `seq 0 7`;
+# do
+# 	for j in `seq 0 124`; #total number is 1000
+# 	do
+# 		kw_list=$kw_list" "$kKeyword10GroupBase"2_"$i"_"$j
+# 	done
+# done
+# ./diane_client -q -b $db_file $kw_list
+#
+# kw_list=""
+# for i in `seq 0 7`;
+# do
+# 	for j in `seq 0 124`; #total number is 1000
+# 	do
+# 		kw_list=$kw_list" "$kKeyword10GroupBase"3_"$i"_"$j
+# 	done
+# done
+# ./diane_client -q -b $db_file $kw_list
+#
+# kw_list=""
+# for i in `seq 0 7`;
+# do
+# 	for j in `seq 0 124`;
+# 	do
+# 		kw_list=$kw_list" "$kKeyword10GroupBase"4_"$i"_"$j
+# 	done
+# done
+# ./diane_client -q -b $db_file $kw_list
+#
+# kw_list=""
+# for i in `seq 0 7`;
+# do
+#         for j in `seq 0 124`;
+#         do
+#                 kw_list=$kw_list" "$kKeyword10GroupBase"5_"$i"_"$j
+#         done
+# done
+# ./diane_client -q -b $db_file $kw_list
 
 # 'Random' groups
+
+# 1e2
+kw_list=""
+for i in `seq 0 7`;
+do
+	for j in `seq 0 103`;
+	do
+		kw_list=$kw_list" "$kKeywordRand10GroupBase"2_"$i"_"$j
+	done
+done
+./diane_client -q -b $db_file $kw_list
 
 # 1e3
 kw_list=""
--- script_bench/graph_gen.py
+++ script_bench/graph_gen.py
@@ -5,31 +5,31 @@ import argparse
 
 
 
-def parse_file(filepath):
+def parse_file(f):
     stats = dict()
-    with open(filepath) as f:
-        lines = f.readlines()
-        for l in lines:
-            tokens = l.strip('\n').split(' ')
-            keyword = tokens[0]
-            value = float(tokens[2])
-            # print tokens
-            # print 'keyword: {0}'.format(keyword)
-            # print 'value: {0}'.format(value)
+    # with open(filepath) as f:
+    lines = f.readlines()
+    for l in lines:
+        tokens = l.strip('\n').split(' ')
+        keyword = tokens[0]
+        value = float(tokens[2])
+        # print tokens
+        # print 'keyword: {0}'.format(keyword)
+        # print 'value: {0}'.format(value)
+        
+        if keyword in stats:
+            old_sum = stats[keyword]["sum"]
+            old_square_sum = stats[keyword]["square_sum"]
+            counter = stats[keyword]["counter"]
+        else:
+            old_sum = 0
+            old_square_sum = 0
+            counter = 0
+            stats[keyword] = dict()
             
-            if keyword in stats:
-                old_sum = stats[keyword]["sum"]
-                old_square_sum = stats[keyword]["square_sum"]
-                counter = stats[keyword]["counter"]
-            else:
-                old_sum = 0
-                old_square_sum = 0
-                counter = 0
-                stats[keyword] = dict()
-                
-            stats[keyword]["sum"] = old_sum+value
-            stats[keyword]["square_sum"] = old_square_sum + value*value
-            stats[keyword]["counter"] = counter+1
+        stats[keyword]["sum"] = old_sum+value
+        stats[keyword]["square_sum"] = old_square_sum + value*value
+        stats[keyword]["counter"] = counter+1
     
     return stats
     
@@ -43,25 +43,25 @@ def compute_mean_var(stats):
     return res
 
 
-def print_mean_var(stats, filepath):
-    with open(filepath, 'w') as f:
-        for key in sorted(stats.iterkeys()):
-            line = '{0} \t {1} \t {2}\n'.format(key, stats[key]["mean"], stats[key]["var"])
-            f.write(line)
+def print_mean_var(stats, f):
+    # with open(filepath, 'w') as f:
+    for key in sorted(stats.iterkeys()):
+        line = '{0} \t {1} \t {2}\n'.format(key, stats[key]["mean"], stats[key]["var"])
+        f.write(line)
     f.close()
             
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process a Diane server benchmark file.')
     
-    parser.add_argument("-i", "--input", dest="in_file",nargs=1, required=True, help ="Input file")
-    parser.add_argument("-o", "--out", dest="out_file", nargs=1, required=True, help ="Output file") 
+    parser.add_argument("-i", "--input", dest="in_file",nargs=1, required=True, help ="Input file", type=argparse.FileType('r'))
+    parser.add_argument("-o", "--out", dest="out_file", nargs=1, required=True, help ="Output file", type=argparse.FileType('w')) 
     args = parser.parse_args()
-    
-    print "Parsing {0}, and outputting in {1}".format(args.in_file, args.out_file)
-    stats = parse_file(args.in_file)
+        
+    print "Parsing {0}, and outputting in {1}".format(args.in_file[0].name, args.out_file[0].name)
+    stats = parse_file(args.in_file[0])
     means_var = compute_mean_var(stats)
     # print stats
     # print "\n\n"
-    print_mean_var(means_var,args.out_file)
+    print_mean_var(means_var,args.out_file[0])
     print "Done"

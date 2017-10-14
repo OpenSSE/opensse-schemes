8d6151176af050da9840cdef6c20f9339ac1a5fb
Raphael Bost <raphael_bost@alumni.brown.edu>
2017-10-14 12:05:09 +0200

    Fixed the protobuf compilation

--- install_grpc.sh
+++ install_grpc.sh
@@ -5,8 +5,8 @@ git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
 
 cd grpc
 git submodule update --init
-make
+make -j2
 
-(cd grpc/third_party/protobuf; sudo make install;)
+(cd third_party/protobuf; sudo make install;)
 
 sudo make install

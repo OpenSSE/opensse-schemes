edacda5ae96543e6d9b1c66815bf6ebd2b548b82
Raphael Bost <raphael_bost@alumni.brown.edu>
2018-11-23 08:31:23 +0100

    Debug

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -4,4 +4,7 @@ mkdir -p build
 cd build
 CMAKE_PREFIX_PATH=${HOME}/deps cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
 # VERBOSE=1 cmake --build . --clean-first
+
+#Show the first 80 lines of the make file
+head -n 80 lib/CMakeFiles/runners.dir/build.make
 make

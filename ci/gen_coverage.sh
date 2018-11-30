ea16792092723b793a187722899584fd02a9c885
Raphael Bost <raphael_bost@alumni.brown.edu>
2018-11-30 18:18:12 +0100

    New CI features. Upload the coverage stats to Coveralls. Update the .travis.yml file to support future evolutions (such as static analysis).

---
+++ ci/gen_coverage.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+set -ex
+
+cd build
+make lcov-geninfo
---
+++ ci/upload_report.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+set -ex
+
+lcov --list build/lcov/data/capture/all_targets.info  # debug before upload
+coveralls-lcov build/lcov/data/capture/all_targets.info 

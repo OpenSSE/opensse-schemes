4ab942ab1e100cc756c5276813f7d4402c380681
Raphael Bost <raphael_bost@alumni.brown.edu>
2018-12-01 00:00:03 +0100

    For the moment, do not clone a fixed tag as the lastest tag (5.17.2) does not compile correctly.

--- ci/install_rocksdb.sh
+++ ci/install_rocksdb.sh
@@ -9,12 +9,12 @@ green=$(tput setaf 2)
 if [ -d "$INSTALL_DIR/include/rocksdb" ] && ([ -f "$INSTALL_DIR/lib/librocksdb.a" ] || [ -f "$INSTALL_DIR/lib/librocksdb.so" ]); then
 	echo "${bold}${green}RocksDB is already installed${normal}"
 else
-	
+
 	echo "${bold}${green}Install RocksDB${normal}"
-	
+
 	set -ex
 
-	git clone -b v5.17.2 --single-branch --depth 1 https://github.com/facebook/rocksdb.git
+	git clone --single-branch --depth 1 https://github.com/facebook/rocksdb.git
 	cd rocksdb
 
 	make INSTALL_PATH=$INSTALL_DIR shared_lib -j4

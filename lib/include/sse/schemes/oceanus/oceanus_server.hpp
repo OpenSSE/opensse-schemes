b19ba15214d24788dbb59ac58c94e8baa8e29bd7
Raphael Bost <raphael_bost@alumni.brown.edu>
2021-06-03 16:04:02 +0200

    Add tests for the cuckoo-based hash table

--- lib/include/sse/schemes/oceanus/oceanus_server.hpp
+++ lib/include/sse/schemes/oceanus/oceanus_server.hpp
@@ -20,12 +20,16 @@ public:
     using content_type       = data_type<PAGE_SIZE>;
     using content_serializer = OceanusContentSerializer<PAGE_SIZE>;
 
+    using get_callback_type
+        = std::function<void(std::experimental::optional<content_type>)>;
+
 
     OceanusServer(const std::string& db_path);
     ~OceanusServer();
 
-    std::vector<index_type> search(const SearchRequest& req);
-    std::vector<index_type> search_async(const SearchRequest& req);
+    data_type<PAGE_SIZE> get(const std::array<uint8_t, kTableKeySize>& ht_key);
+    void async_get(const std::array<uint8_t, kTableKeySize>& ht_key,
+                   get_callback_type                         callback);
 
     // using content_type = payload_type<PAGE_SIZE>;
     // using content_type = typename
@@ -55,91 +59,92 @@ OceanusServer<PAGE_SIZE>::~OceanusServer()
 }
 
 template<size_t PAGE_SIZE>
-std::vector<index_type> OceanusServer<PAGE_SIZE>::search(
-    const SearchRequest& req)
+data_type<PAGE_SIZE> OceanusServer<PAGE_SIZE>::get(
+    const std::array<uint8_t, kTableKeySize>& ht_key)
 {
-    std::vector<index_type> res;
+    // std::vector<index_type> res;
 
-    for (size_t i = 0;; i++) {
-        // generate the table key
-        std::array<uint8_t, kTableKeySize> prf_out
-            = req.prf.prf(reinterpret_cast<uint8_t*>(&i), sizeof(i));
 
-        try {
-            // query the table
-            content_type val = cuckoo_table.get(prf_out);
+    // query the table
+    content_type val = cuckoo_table.get(ht_key);
 
-            for (auto it = val.begin(); it != val.end(); ++it) {
-                // 'deserialize' the results
-                res.push_back(*it);
-            }
-        } catch (const std::out_of_range& e) {
-            std::cerr << e.what() << '\n';
-            break;
-        }
-    }
-    return res;
+    // for (auto it = val.begin(); it != val.end(); ++it) {
+    //     // 'deserialize' the results
+    //     res.push_back(*it);
+    // }
+    return val;
 }
 
 
 template<size_t PAGE_SIZE>
-std::vector<index_type> OceanusServer<PAGE_SIZE>::search_async(
-    const SearchRequest& req)
+void OceanusServer<PAGE_SIZE>::async_get(
+    const std::array<uint8_t, kTableKeySize>& ht_key,
+    get_callback_type                         callback)
 {
     cuckoo_table.use_direct_IO(true);
 
-    std::vector<index_type> res;
-
-    std::atomic<bool>  stop_flag(false);
-    std::atomic_size_t submitted_queries(0);
-    std::atomic_size_t completed_queries(0);
-
-    std::promise<void> notifier;
-    std::future<void>  notifier_future = notifier.get_future();
-
-    std::mutex mutex;
-
-    auto callback = [&notifier,
-                     &res,
-                     &stop_flag,
-                     &submitted_queries,
-                     &completed_queries,
-                     &mutex](std::experimental::optional<content_type> val) {
-        if (val.has_value()) {
-            // the callback can be called from two different threads, so we need
-            // a lock here
-            const std::lock_guard<std::mutex> lock(mutex);
-            for (auto it = val.value().begin(); it != val.value().end(); ++it) {
-                // 'deserialize' the results
-                res.push_back(*it);
-            }
-        } else {
-            // value not found, any new query is useless
-            stop_flag.store(true);
-        }
-        size_t query_count = completed_queries.fetch_add(1) + 1;
-
-        if ((query_count == submitted_queries) && stop_flag) {
-            notifier.set_value();
-        }
-    };
-
-
-    for (size_t i = 0; !stop_flag; i++) {
-        // generate the table key
-        std::array<uint8_t, kTableKeySize> prf_out
-            = req.prf.prf(reinterpret_cast<uint8_t*>(&i), sizeof(i));
-
-        cuckoo_table.async_get(prf_out, callback);
-        submitted_queries.fetch_add(1);
-    }
-
-    // wait for completion of the queries
-    notifier_future.get();
-
-
-    return res;
+    cuckoo_table.async_get(ht_key, callback);
 }
+
+// template<size_t PAGE_SIZE>
+// std::vector<index_type> OceanusServer<PAGE_SIZE>::search_async(
+//     const SearchRequest& req)
+// {
+//     cuckoo_table.use_direct_IO(true);
+
+//     std::vector<index_type> res;
+
+//     std::atomic<bool>  stop_flag(false);
+//     std::atomic_size_t submitted_queries(0);
+//     std::atomic_size_t completed_queries(0);
+
+//     std::promise<void> notifier;
+//     std::future<void>  notifier_future = notifier.get_future();
+
+//     std::mutex mutex;
+
+//     auto callback = [&notifier,
+//                      &res,
+//                      &stop_flag,
+//                      &submitted_queries,
+//                      &completed_queries,
+//                      &mutex](std::experimental::optional<content_type> val) {
+//         if (val.has_value()) {
+//             // the callback can be called from two different threads, so
+//             // we need a lock here
+//             const std::lock_guard<std::mutex> lock(mutex);
+//             for (auto it = val.value().begin(); it != val.value().end();
+//             ++it) {
+//                 // 'deserialize' the results
+//                 res.push_back(*it);
+//             }
+//         } else {
+//             // value not found, any new query is useless
+//             stop_flag.store(true);
+//         }
+//         size_t query_count = completed_queries.fetch_add(1) + 1;
+
+//         if ((query_count == submitted_queries) && stop_flag) {
+//             notifier.set_value();
+//         }
+//     };
+
+
+//     for (size_t i = 0; !stop_flag; i++) {
+//         // generate the table key
+//         std::array<uint8_t, kTableKeySize> prf_out
+//             = req.prf.prf(reinterpret_cast<uint8_t*>(&i), sizeof(i));
+
+//         cuckoo_table.async_get(prf_out, callback);
+//         submitted_queries.fetch_add(1);
+//     }
+
+//     // wait for completion of the queries
+//     notifier_future.get();
+
+
+//     return res;
+// }
 
 
 } // namespace oceanus
